c04d7a36ea6004b776949eb98bf95cb0db25f804
unnathi konduru <ukonduru@umd.edu>
2024-02-28 22:00:56 -0500

    Modified the app.py file to add a new route for the About CSS page

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def hello():
 def about():
     return render_template('about.html', hello={'html': 'hello.html'})
 
+@app.route('/about-css')
+def about_css():
+    return render_template('about-css.html')
 
 if __name__ == '__main__':
     app.run()
