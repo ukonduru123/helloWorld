4eb3e3321178a55eab14df0fffeeb52cd7f29d98
unnathi konduru <ukonduru@umd.edu>
2024-03-27 23:34:35 -0400

    Added new favorite course and contact routes with related fields

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +16,27 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        date_of_birth = request.form['date_of_birth']
+        return render_template('contact.html',
+                               submitted=True,
+                               first_name=first_name,
+                               last_name=last_name,
+                               email=email,
+                               date_of_birth=date_of_birth)
+    return render_template('contact.html', submitted=False)
 
 if __name__ == '__main__':
     app.run()
